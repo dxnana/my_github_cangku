3f5b2510cd041829c49a9173c0477c12e6e4295f
rui.wei <18855584523@163.com>
2017-08-11 15:12:24 +0800

    first commit

---
+++ 网易严选/src/pages/homeLink/jujia.vue
@@ -0,0 +1,22 @@
+<<template>
+        <div>
+                <h3>{{title}}</h3>
+        </div>
+</template>
+
+<script>
+        export default {
+                name:"jujia",
+                data:function(){
+                        return {
+                                title:"居家"
+                        }
+                }
+
+        }
+</script>
+
+<style>
+
+</style>
+
